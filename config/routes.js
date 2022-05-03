7efef63288d12bfb17f6da72d8f28953e1ed012b
danielgsantos15 <danir877@gmail.com>
2022-05-03 16:42:24 -0300

    feat: page redirect created

--- config/routes.js
+++ config/routes.js
@@ -5,6 +5,17 @@ const regex = require('regex')
 const match = require('nodemon/lib/monitor/match')
 const Link = require('../models/link.js')
 
+router.get('/:code', async (req, res) => {
+    const code = req.params.code
+
+    const resultado = await Link.findOne({ where: { code } })
+    if (!resultado) return res.sendStatus(404)
+
+    await resultado.save()
+
+    res.redirect(resultado.url)
+})
+
 router.get('/', (req, res) => {
     if(validUrl.isUri(req.headers.url)){
         console.log(req.headers.url)
@@ -28,6 +39,7 @@ generateUrl()
 router.post('/new', async (req, res) => {
     const url = req.body.url
     const code = generateUrl()
+    console.log(code)
 
     const resultado = await Link.create({
         url,
