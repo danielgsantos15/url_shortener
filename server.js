bada6e6ce2779185f5019d3bb7dca05f9dc022ad
danielgsantos15 <danir877@gmail.com>
2022-05-03 14:28:19 -0300

    feat: database links added

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const router = require('./config/routes')
-const app = express()
+(async () => {
+    const database = require('./db')
+    const Link = require('./models/link')
 
-app.use(morgan('dev'))
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(express.json())
-app.use(cors())
-app.use(router)
+    await database.sync()
+
+    const express = require('express')
+    const morgan = require('morgan')
+    const cors = require('cors')
+    const bodyParser = require('body-parser')
+    const router = require('./config/routes')
+    const app = express()
+
+    app.use(morgan('dev'))
+    app.use(bodyParser.urlencoded({ extended: false }))
+    app.use(express.json())
+    app.use(cors())
+    app.use(router)
 
 
-app.listen(3000, () => {
-    console.log(`Express started at http://localhost:3000`)
-})
+    app.listen(3000, () => {
+        console.log(`Express started at http://localhost:3000`)
+    })
+})()
