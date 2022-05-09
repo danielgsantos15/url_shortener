de22a1dc434d7422c6a04c0c53e1cba152bb5a90
danielgsantos15 <danir877@gmail.com>
2022-05-09 16:10:09 -0300

    refactor: identation tab size change to two spaces

--- config/routes.js
+++ config/routes.js
@@ -6,46 +6,46 @@ const match = require('nodemon/lib/monitor/match')
 const Link = require('../models/link.js')
 
 router.get('/:code', async (req, res) => {
-    const code = req.params.code
+  const code = req.params.code
 
-    const resultado = await Link.findOne({ where: { code } })
-    if (!resultado) return res.sendStatus(404)
+  const resultado = await Link.findOne({ where: { code } })
+  if (!resultado) return res.sendStatus(404)
 
-    await resultado.save()
+  await resultado.save()
 
-    res.redirect(resultado.url)
+  res.redirect(resultado.url)
 })
 
 router.get('/', (req, res) => {
-    if(validUrl.isUri(req.headers.url)){
-        console.log(req.headers.url)
-    }
-    else {
-        console.log('Not a URL')
-    }
-    res.header(201)
-    res.end()
+  if (validUrl.isUri(req.headers.url)) {
+    console.log(req.headers.url)
+  }
+  else {
+    console.log('Not a URL')
+  }
+  res.header(201)
+  res.end()
 })
 
 function generateUrl() {
-    let text = ''
-    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-    for(let i = 0; i < 5; i++)
+  let text = ''
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  for (let i = 0; i < 5; i++)
     text += possible.charAt(Math.floor(Math.random() * possible.length))
-    return text
+  return text
 }
 generateUrl()
 
 router.post('/new', async (req, res) => {
-    const url = req.body.url
-    const code = generateUrl()
-    console.log(code)
+  const url = req.body.url
+  const code = generateUrl()
+  console.log(code)
 
-    const resultado = await Link.create({
-        url,
-        code
-    })
-    res.send(resultado)
+  const resultado = await Link.create({
+    url,
+    code
+  })
+  res.send(resultado)
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 (async () => {
-    const database = require('./db')
-    const Link = require('./models/link')
+  const database = require('./db')
+  const Link = require('./models/link')
 
-    await database.sync()
+  await database.sync()
 
-    const express = require('express')
-    const morgan = require('morgan')
-    const cors = require('cors')
-    const bodyParser = require('body-parser')
-    const router = require('./config/routes')
-    const app = express()
+  const express = require('express')
+  const morgan = require('morgan')
+  const cors = require('cors')
+  const bodyParser = require('body-parser')
+  const router = require('./config/routes')
+  const app = express()
 
-    app.use(morgan('dev'))
-    app.use(bodyParser.urlencoded({ extended: false }))
-    app.use(express.json())
-    app.use(cors())
-    app.use(router)
+  app.use(morgan('dev'))
+  app.use(bodyParser.urlencoded({ extended: false }))
+  app.use(express.json())
+  app.use(cors())
+  app.use(router)
 
 
-    app.listen(3000, () => {
-        console.log(`Express started at http://localhost:3000`)
-    })
+  app.listen(3000, () => {
+    console.log(`Express started at http://localhost:3000`)
+  })
 })()
